6c779fd0e17880da9b4afe7895c104a898011e4c
Nick_Cheung <nkkcheung@gmail.com>
2020-09-01 16:40:00 -0700

    beginning

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-
-// // // console.log(d3)
+// console.log(d3)
 // import mydata from './jss/SanFranciscoCA'
 // import * as data from './js db/SanFranciscoCA.js';
-// const sfaqi = require("./js db/SanFranciscoCA.json")
-const SanFranciscoCA = [
+// const sfData = require("./js db/SanFranciscoCA.json")
+const sfData = [
  {
    "date": "2019/7/2",
    "pm25": 35,
@@ -6500,87 +6499,54 @@ const SanFranciscoCA = [
    "so2": null,
    "co": null
  }
-]
+];
+
+
+const xScale = d3.scaleBand().domain(sfData.map(dataPoint => dataPoint.date)).rangeRound([0,250]).padding(0.1);
+//ordinal scale, all of them uniform
+const yScale = d3.scaleLinear().domain([-1000, 1500]).range([2000, 1000]);
+//gives us different values to translate vals to other num
 
 
 
-// let myData = require('./SanFranciscoCA.json')
-// console.log(myData[0])
+const container = d3.select('svg')
+        .classed('container', true) //gives it a class name
+        // .style('border', '1px solid red');
 
-
-// function load() {
-//     let aqi = JSON.parse(dataSF);
-//     console.log(aqi[0]);
-// }
-// load();
-
-// function loadJSON(cb) {
-//     let sfaqi = new XMLHttpRequest();
-//     sfaqi.overrideMimeType("application.json");
-//     sfaqi.open("GET", "./js db/SanFranciscoCA.json");
-//     sfaqi.onreadystatechange = function () {
-//         if (sfaqi.readyState == 4 && sfaqi.status == "200") {
-//             cb(JSON.parse(sfaqi.responseText));
-//         }
-//     }
-//     sfaqi.send(null)
-// }
-
-// loadJSON(function(db) {
-//     console.log(db)
-// });
-// console.log(loadJSON(db))
-
-
-
-// // const sfURL = './js db/SanFranciscoCA.json';
-// // let http = require('http');
-
-
-// const xScale = d3.scaleBand().domain(sfaqi.map(dataPoint => dataPoint.date)).rangeRound([0,250]).padding(0.1);
-// //ordinal scale, all of them uniform
-// const yScale = d3.scaleLinear().domain([0, 15]).range([200, 0]);
-// //gives us different values to translate vals to other num
-
-
-
-// const container = d3.select('svg')
-//         .classed('container', true) //gives it a class name
-//         // .style('border', '1px solid red');
-
-// const bars = container
-//     .selectAll('.bar')
-//     .data(sfaqi)
-//     .enter()
-//     .append('rect') //valid ele
-//     .classed('bar', true)
-//     .text(d => d)
-//     .attr('width', xScale.bandwidth())
-//     .attr('height', data => 200 - yScale(data.pm25))
-//     .attr('x', data => xScale(data.date))
-//     .attr('y', data => yScale(data.pm25))
+const bars = container
+    .selectAll('.bar')
+    .data(sfData)
+    .enter()
+    .append('rect') //valid ele
+    .classed('bar', true)
+    .text(d => d)
+    .attr('width', xScale.bandwidth())
+    .attr('height', data => 200 - yScale(data.pm25))
+    .attr('x', data => xScale(data.date))
+    .attr('y', data => yScale(data.pm25))
 
 // // setTimeout(() => {
 // //     bars.data(fake.slice(0, 2)).exit().remove();
 // // }, 2000)
 
-// // const text = bars.selectAll('text')
-// //     .data(fake)
-// //     .enter()
-// //     .append("text")
-// //     .text(d =>( d.region))
-// //     // .attr('x', data => xScale -)
-// //     // .attr('y', data => )
+const text = bars.selectAll('text')
+    .data(sfData)
+    .enter()
+    .append("text")
+    .text(d =>( d.pm25))
+    // .attr('x', data => xScale -)
+    // .attr('y', data => )
 
 
 
-// // // d3.select('div') //the class div or whatever
-// // //     .selectAll('p') //all paragraphgs
-// // //     .data(fake) //can be araray or full of strings //binds data
-// // //     .enter() //which are missing?
-// // //     .append('p') //appends p for all missing
-// // //     .text( dta => dta.region) //dta gives access to each data node
-// // //     // .selectAll();
+d3.select('div') //the class div or whatever
+    .selectAll('p') //all paragraphgs
+    .data(sfData) //can be araray or full of strings //binds data
+    .enter() //which are missing?
+    .append('p') //appends p for all missing
+    .text( dta => dta.pm25) //dta gives access to each data node
+    
+    // .selectAll();
 
 // // const fake = [
 // //     {id: 'd1', value: 10, region: 'USA'},
@@ -6592,12 +6558,12 @@ const SanFranciscoCA = [
 // //     {id: 'd7', value: 6, region: 'US123A'},
 // // ];
 
-// // import sfaqi from './js db/SanFranciscoCA.json';
+// // import sfData from './js db/SanFranciscoCA.json';
 // // import beijing from './js db/beijing-air-quality.json';
 // // import paris from './js db/paris-air-quality.json';
 // // const fetch = require('node-fetch');
 
-console.log("hello", SanFranciscoCA[0].date)
+// console.log("hello", SanFranciscoCA[0].date)
 
 
 // var mydata = JSON.parse(SanFranciscoCA)
--- rand.js
+++ rand.js
@@ -1,22 +1,56 @@
-const sfURL = './js db/SanFranciscoCA.json';
-const settings = { method: 'GET' };
-// fetch(sfURL, settings)
-//     .then(res => res.json())
-//     console.log(res)
-https.get(url, (res) => {
-    let body = "";
+// const sfURL = './js db/SanFranciscoCA.json';
+// const settings = { method: 'GET' };
+// // fetch(sfURL, settings)
+// //     .then(res => res.json())
+// //     console.log(res)
+// https.get(url, (res) => {
+//     let body = "";
 
-    res.on("data", (chunk) => {
-        body += chunk;
-    });
+//     res.on("data", (chunk) => {
+//         body += chunk;
+//     });
 
-    res.on("end", () => {
-        try {
-            let json = JSON.parse(body);
-        } catch (error) {
-            console.log(error.message)
-        };
-    }).on("error", (error) => {
-        console.error(error.message);
-    })
-})
+//     res.on("end", () => {
+//         try {
+//             let json = JSON.parse(body);
+//         } catch (error) {
+//             console.log(error.message)
+//         };
+//     }).on("error", (error) => {
+//         console.error(error.message);
+//     })
+// })
+
+// let myData = require('./SanFranciscoCA.json')
+// console.log(myData[0])
+
+
+// function load() {
+//     let aqi = JSON.parse(dataSF);
+//     console.log(aqi[0]);
+// }
+// load();
+
+// function loadJSON(cb) {
+//     let sfaqi = new XMLHttpRequest();
+//     sfaqi.overrideMimeType("application.json");
+//     sfaqi.open("GET", "./js db/SanFranciscoCA.json");
+//     sfaqi.onreadystatechange = function () {
+//         if (sfaqi.readyState == 4 && sfaqi.status == "200") {
+//             cb(JSON.parse(sfaqi.responseText));
+//         }
+//     }
+//     sfaqi.send(null)
+// }
+
+// loadJSON(function(db) {
+//     console.log(db)
+// });
+// console.log(loadJSON(db))
+
+
+
+// // const sfURL = './js db/SanFranciscoCA.json';
+// // let http = require('http');
+
+
