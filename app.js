ea5d7d90b59e05daba204071ab2044520ea473dd
Nick_Cheung <nkkcheung@gmail.com>
2020-09-23 14:25:35 -0700

    successful port v6

--- app.js
+++ app.js
@@ -1,80 +1,143 @@
-// // console.log(d3)
-
-// let container = d3.select('svg')
-//     .classed('container', true);
-
-let margin = {top: 10, right: 30, bottom: 30, left: 50},
+const margin = {top: 10, right: 30, bottom: 30, left: 50},
+// const data = "https://raw.githubusercontent.com/cowfish813/D3.js/master/csv%20files/san-francisco%2C%20california%2C%20usa-air-quality.csv"
   width = 1080 - margin.left - margin.right
   height = 400 - margin.top - margin.bottom
 
-let svg = d3.select('#my_dataviz')
+const svg = d3.select('#my_dataviz')
     .append("svg") //adds svg ele
       .attr("width", width + margin.left + margin.right)
       .attr("height", height + margin.top + margin.bottom)
       .append("g")
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-// const data = d3.csv("https://raw.githubusercontent.com/cowfish813/D3.js/master/csv%20files/san-francisco%2C%20california%2C%20usa-air-quality.csv")
-// const data = "https://raw.githubusercontent.com/cowfish813/D3.js/master/csv%20files/san-francisco%2C%20california%2C%20usa-air-quality.csv"
 
-const parseTime = d3.timeParse()
+const parseTime = d3.timeParse("%Y/%m/%d");
+
+const x = d3.scaleTime()
+  // .domain(d3.extent(data, (d) => { return d.date; })) //no data yet, append later?
+  .range([0, width]);
+
+const y = d3.scaleLinear()
+  // .domain([0, 240]) //use a Math.max(data.)something instead of 2nd arg
+  .range([height, 0]);
+
+
+  //might not be necesesary anymore?
+const valueLine = d3.line()
+  .x((d) => (x(d.date)))
+  .y((d) => (y(d[" pm25"])));
+
+
+d3.csv("https://raw.githubusercontent.com/cowfish813/D3.js/master/csv%20files/san-francisco%2C%20california%2C%20usa-air-quality.csv").then((data) => {
+  // console.log(data)
+  data.forEach(d => {
+    d.date = parseTime(d.date);
+    d.pm25 = d[" pm25"];
+  });
+
+  x.domain(d3.extent(data, (d) => { return d.date; })) //no data yet, append later?
+  y.domain([0, 240]) //use a Math.max(data.)something instead of 2nd arg
 
 
 
+  
+  // svg.selectAll("dot")
+  //     .data(data)
+  //   .enter().append("circle")
+  //     .attr("r", 5)
+  //     .attr("cx", d => (x(d.date)))
+  //     .attr("cy", d => (y(d[" pm25"])))
+  //     .attr("fill", "#69b3a2") //color
+  
+  svg.append("g")
+  .attr("transform", "translate(0," + height + ")")
+  .call(d3.axisBottom(x));
+  
+  svg.append("g")
+  .call(d3.axisLeft(y))
+  
+  // Add the valueline path.
+  // svg.append("path")
+  //   .data([data])
+  //   .attr("class", "line")
+  //   .attr("d", valueLine)
+    // .attr("fill", "#69b3a2") //color
+  // Add the valueline path.
 
-d3.csv("https://raw.githubusercontent.com/cowfish813/D3.js/master/csv%20files/san-francisco%2C%20california%2C%20usa-air-quality.csv").then(function(d, error) {
-  // if (error) throw error,
-  // console.log(d),
-  // function (d) {
-  //   // console.log(d3.timeParse("%Y/%m/%d")(d.date))
-  //   return { date: d3.timeParse("%Y/%m/%d")(d.date), pm25: d[" pm25"] } //in brackets because it's a string
-  // },
-  (d) => {
-    return { date: d3.timeParse("2018/%m/%d")(d.date), pm25: d[" pm25"] } //change year
-  },
+  // Add the line
+  svg
+    .append("path")
+    .datum(data)
+    .attr("fill", "none")
+    .attr("stroke", "#69b3a2")
+    .attr("stroke-width", 1.5)
+    .attr("d", d3.line()
+      .x((d) => { return x(d.date) })
+      .y((d) => { return y(d.pm25) })
+    )
 
-  // Now I can use this dataset:
-  (data) => {
-    // console.log(data)
-    // Add X axis --> it is a date format
-    const x = d3.scaleTime()
-      .domain(d3.extent(data, (d) => { return d.date; }))
-      .range([0, width]);
-
-    svg.append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x));
-    // Add Y axis
-    const y = d3.scaleLinear()
-      .domain([0, 240]) //use a Math.max(data.)something instead of 2nd arg
-      .range([height, 0]);
-
-    svg.append("g")
-      .call(d3.axisLeft(y));
-      
-    // Add the line
-    svg
-      .append("path")
-      .datum(data)
-      .attr("fill", "none")
-      .attr("stroke", "#69b3a2")
-      .attr("stroke-width", 1.5)
-      .attr("d", d3.line()
-        .x((d) => { return x(d.date) })
-        .y((d) => { return y(d.pm25) })
-      )
     // Add the points
-    svg
-      .append("g")
-      .selectAll("dot")
-      .data(data)
-      .enter()
-      .append("circle")
-      .attr("cx", (d) => { return x(d.date) })
-      .attr("cy", (d) => { return y(d.pm25) })
-      .attr("r", 5)
-      .attr("fill", "#69b3a2")
-  }});
+  svg
+    .append("g")
+    .selectAll("dot")
+    .data(data)
+    .enter()
+    .append("circle")
+    .attr("cx", (d) => { return x(d.date) })
+    .attr("cy", (d) => { return y(d.pm25) })
+    .attr("r", 5)
+    .attr("fill", "#69b3a2")
+});
+
+// d3.csv("https://raw.githubusercontent.com/cowfish813/D3.js/master/csv%20files/san-francisco%2C%20california%2C%20usa-air-quality.csv").then(function(d, error) {
+//   // if (error) throw error,
+//   // console.log(d),
+//   // function (d) {
+//   //   // console.log(d3.timeParse("%Y/%m/%d")(d.date))
+//   //   return { date: d3.timeParse("%Y/%m/%d")(d.date), pm25: d[" pm25"] } //in brackets because it's a string
+//   // },
+//   (d) => {
+//     return { date: d3.timeParse("2018/%m/%d")(d.date), pm25: d[" pm25"] } //change year
+//   },
+
+//   // Now I can use this dataset:
+//   (data) => {
+//     // console.log(data)
+//     // Add X axis --> it is a date format
+
+
+//     svg.append("g")
+//       .attr("transform", "translate(0," + height + ")")
+//       .call(d3.axisBottom(x));
+//     // Add Y axis
+
+
+//     svg.append("g")
+//       .call(d3.axisLeft(y));
+      
+//     // Add the line
+//     svg
+//       .append("path")
+//       .datum(data)
+//       .attr("fill", "none")
+//       .attr("stroke", "#69b3a2")
+//       .attr("stroke-width", 1.5)
+//       .attr("d", d3.line()
+//         .x((d) => { return x(d.date) })
+//         .y((d) => { return y(d.pm25) })
+//       )
+//     // Add the points
+//     svg
+//       .append("g")
+//       .selectAll("dot")
+//       .data(data)
+//       .enter()
+//       .append("circle")
+//       .attr("cx", (d) => { return x(d.date) })
+//       .attr("cy", (d) => { return y(d.pm25) })
+//       .attr("r", 5)
+//       .attr("fill", "#69b3a2")
+//   }});
 
 
 
