cab4a7b218ad3c3afa4c595c89108e14cac3c4ac
Nick_Cheung <nkkcheung@gmail.com>
2020-10-06 19:42:39 -0700

    present

--- app.js
+++ app.js
@@ -66,7 +66,6 @@ const svg = d3.select('#my_dataviz')
       .append("g")
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
       
-      
 const parseTime = d3.timeParse(`%m/%d`);
 const y = d3.scaleLinear().range([height, 0]);
 const x = d3.scaleTime().range([0, width]);
@@ -105,12 +104,79 @@ d3.csv(test)
   const colors = d3.scaleOrdinal()
     .domain(years)
     .range(d3.schemeSet2);
-    
 
+  const line = d3.line()
+    .x(d => { return x(d.date)})
+    .y(d => { return y(d.pm25)});
 
+    const labels = svg
+      .selectAll("labels")
+      .data(aData)
+      .enter()
+      .append("g")
+      .append("text")
+      // .datum(d => {
+      //   return ({
+      //   year: d.key,
+      //   value: d.values[d.values.length - 1] 
+      //   })
+      // })
+  
+  const showLine = (e, selectedLine) => {
+    const selected = selectedLine.year.trim();
+    const pm25 = selectedLine.pm25;
 
-/////////////////////
-    // dot mouseover events
+    lines.attr("d", d => {
+      // console.log(d)
+      if (selected === d.key) {
+
+        labels.attr("x", 12)
+          .text(d => { return (`Year: ${selected}`) })
+          // .style("fill", d => { return (colors(d.year)) })
+          .style("font-size", 15)
+          .attr('opacity', '10')
+          // .style("position", "relative")
+          // .style("opacity", 1)
+          .attr("transform",
+             ("translate(" + x(selectedLine.date) + "," + y(selectedLine.pm25) + ")")
+          );
+        return line(d.values)
+      };
+    })
+    .attr("stroke", d => { return colors(d.key) })
+    .attr("stroke-width", 3)
+    .attr("fill", "none");
+  };
+
+  const lines = svg.selectAll("lines")
+    .data(aData, d => {
+      // console.log(d)
+    return {
+      year: d.key,
+      value: d.values
+    }})
+    .enter()
+    .append("path")
+    .attr('opacity', '.9');
+      
+  const dots = svg.append("g")
+    .selectAll("dot")
+    .data(data, d => {
+      return {
+        date: d.date,
+      }
+    })
+    .enter()
+    .append("circle")
+      .attr("r", 2) //radius
+      .attr("cx", d => (x(d.date)))
+      .attr("cy", d => (y(d.pm25)))
+      .attr('opacity', '.7')
+      .style("fill", d => (colors(d.year)))
+      .on("mouseover", showLine)
+
+    /////////////////////
+    // dot mouseover events - prettier than what's present
     const infoWindow = d3.select("g")
       .append("div")
       .attr("class", "window")
@@ -120,12 +186,12 @@ d3.csv(test)
 
     const showInfo = (e, d) => {
       // console.log(d) //mouseover data info
-      const info = 
-      `Year: ${d.year} <br>
+      const info =
+        `Year: ${d.year} <br>
       PM25: ${d.pm25} <br>`
 
       // infoWindow.transition()
-        // .duration(2)
+      // .duration(2)
       infoWindow.style("opacity", 1)
         .html(infoWindow)
         // .style("left", (d3.e.pageX) + "px")
@@ -141,123 +207,6 @@ d3.csv(test)
     }
   ///////////////////////////
 
-  const line = d3.line()
-    .x(d => { return x(d.date)})
-    .y(d => { return y(d.pm25)});
 
-
-
-    const labels = svg
-      .selectAll("labels")
-      .data(aData)
-      .enter()
-      .append("g")
-      .append("text")
-      // .datum(d => {
-      //   return ({
-      //   year: d.key,
-      //   value: d.values[d.values.length - 1] 
-      //   })
-      // })
-      // ERROR CHECK HERE.
-      // .attr("transform", d => { 
-      //   // debugger
-      //   // console.log(d)
-      //   return ("translate(" + x(d.value.pm25) + "," + y(d.value.year) + ")")}) //last value of data point
-      // disappears when attr(transform) comes in
-
-  
-  const showLine = (e, selectedLine) => {
-    const selected = selectedLine.year.trim();
-    const pm25 = selectedLine.pm25;
-
-    lines.attr("d", d => {
-      // console.log(d)
-      if (selected === d.key) {
-        labels.attr("x", 12)
-          .text(d => { return (`Year: ${selected}`) })
-          // .style("fill", d => { return (colors(d.year)) })
-          .style("font-size", 15)
-          // .style("position", "relative")
-          .attr("transform",
-             ("translate(" + x(selectedLine.date) + "," + y(selectedLine.pm25) + ")")
-          );
-        
-        return line(d.values)
-      };
-        
-    })
-    .attr("stroke", d => { return colors(d.key) })
-    .attr("stroke-width", 3)
-    .attr("fill", "none")
-    
-    
-    // dots.attr("g", d => {
-    //   if (selectedLine.date === d.date) {
-    //     // dot
-    //     //   .append("circle")
-    //     //   .attr("r", 5) //radius
-    //     //   .attr("cx", (x(selectedLine.date)))
-    //     //   .attr("cy", (y(selectedLine.pm25)))
-    //     //   .style("stroke", "black")
-    //     //   //WHY THIS NOT WORK?
-
-      //     svg.selectAll("text")
-      //     .append("t")
-      //     .append("text")
-      //       .text( selectedLine.pm25 )
-      //       .attr("x", function (d) {
-      //         return x(selectedLine.date);  // Returns scaled location of x
-      //       })
-      //       .attr("y", function (d) {
-      //         return y(selectedLine.pm25);  // Returns scaled circle y
-      //       })
-      //       .text( "20198" )
-      //       .style("font-size", 105)
-      // .attr("font_family", "sans-serif")  // Font type
-      // .attr("font-size", "11px")  // Font size
-      // .attr("fill", "darkgreen");   // Font color
-    //   } 
-    // })    
-  };
-
-  const dot = svg.append("g")
-    .selectAll("circle")
-    // .data(data, d => {
-    //   return {
-    //     date: d.date,
-    //     pm25: d.pm25,
-    //     year: d.year
-    //   }
-    // })
-    .enter()
-
-  const lines = svg.selectAll("lines")
-    .data(aData, d => {
-      // console.log(d)
-    return {
-      year: d.key,
-      value: d.values
-    }})
-    .enter()
-    .append("path")
-
-
-    
-      
-  const dots = svg.append("g")
-    .selectAll("dot")
-    .data(data, d => {
-      return {
-        date: d.date,
-      }
-    })
-    .enter()
-    .append("circle")
-      .attr("r", 2) //radius
-      .attr("cx", d => (x(d.date)))
-      .attr("cy", d => (y(d.pm25)))
-      .style("fill", d => (colors(d.year)))
-      .on("mouseover", showLine)
 });
 
